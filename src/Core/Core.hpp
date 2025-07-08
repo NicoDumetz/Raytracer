2db54109611173cc9d2c8035975c00e78ff5ea4f
NicoDumetz <nicolas.dumetz@epitech.eu>
2025-07-08 14:12:13 +0200

    feat: main function and libloader class implementation, moved plugin/ at root

---
+++ src/Core/Core.hpp
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2025
+** Projet
+** File description:
+** HitRecord.cpp
+*/
+
+#include "LibLoader/LibLoader.hpp"
+
+#define PLUGIN_PATH std::string("./plugin/")
+
+namespace RayTracer
+{
+class Core
+{
+public:
+    Core();
+    ~Core() = default;
+private:
+    LibLoader _libLoader;
+};
+} // namespace RayTracer
