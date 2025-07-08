6a67adfa5364f9194153f686523237152276019c
NicoDumetz <nicolas.dumetz@epitech.eu>
2025-07-08 14:14:19 +0200

    Merge branch 'main' of github.com:EpitechPromo2028/B-OOP-400-LIL-4-1-raytracer-maximilien.dheripre into feat/clustering

--- src/Interface/IPrimitive.hpp
+++ src/Interface/IPrimitive.hpp
@@ -9,6 +9,7 @@
 
 #include "Tools/Ray/Ray.hpp"
 #include "Tools/HitRecord/HitRecord.hpp"
+#include "Tools/AABB/AABB.hpp"
 #include "Tools/Math/Matrix/TransformMatrix.hpp"
 
 namespace Primitive
@@ -18,6 +19,7 @@ namespace Primitive
         virtual ~IPrimitive() = default;
         virtual bool hit(const Utils::Ray& ray,  Utils::HitRecord& record) const = 0;
         virtual void applyTransform(const math::TransformMatrix &transform) = 0;
+        virtual bool getBoundingBox(Utils::AABB &box) const = 0;
     };
 } // namespace Raytracer
 
